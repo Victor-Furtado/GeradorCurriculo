3c940956ff95dcf5a8ec51f4036275067a01e7d5
stolen <victor.furtado@swisscows.com>
2024-03-08 10:57:09 -0300

    Card Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
     import Header from "./lib/partials/Header.svelte";
     import Footer from "./lib/partials/Footer.svelte";
+
+    import Card from "./lib/components/Card.svelte";
+
+    import UserDocIcon from "./assets/userdoc.icon.svg";
+
+
 </script>
 
 <Header />
 <form class="form">
+    <Card title="Informações Básicas" icon={UserDocIcon}>
+        <input type="text" name="name" id="name">
+        <input type="email" name="email" id="email">
+        <input type="tel" name="telefone" id="telefone">
+    </Card>
 </form>
 <Footer />
 
---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let title: string;
+    export let icon: string | null = null;
+</script>
+
+<div class="card">
+    <div class="card-header">
+        {#if !!icon}
+            <img src={icon} alt={`${title} icon`}>
+        {/if}
+        {title}
+    </div>
+    <p class="card-body">
+        <slot />
+    </p>
+</div>
+
+<style>
+    .card {
+        min-width: 300;
+        height: fit-content;
+        border-radius: 2rem;
+        /* padding: .5rem .8rem; */
+        background-color: var(--color-surface-3);
+    }
+
+    .card-header {
+        border-radius: 2rem 2rem 0 0;
+        padding: 0.5rem 1.2rem;
+        background-color: var(--color-surface-1);
+    }
+
+    .card-body {
+        padding: 0.5rem 1.2rem;
+    }
+</style>
