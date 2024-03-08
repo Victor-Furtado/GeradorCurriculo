56c1a8bb4db721cc64a509e49930d4749fedee11
stolen <victor.furtado@swisscows.com>
2024-03-08 11:00:18 -0300

    Building form

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,25 @@
     import Header from "./lib/partials/Header.svelte";
     import Footer from "./lib/partials/Footer.svelte";
 
+    import UserDocIcon from "./assets/userdoc.icon.svg";
+    import LinkDocIcon from "./assets/linkdoc.icon.svg";
+
     import Card from "./lib/components/Card.svelte";
     import TextInput from "./lib/components/TextInput.svelte";
-
-    import UserDocIcon from "./assets/userdoc.icon.svg";
 </script>
 
 <Header />
 <form class="form">
+    
     <Card title="Informações Básicas" icon={UserDocIcon}>
         <TextInput name="nome" />
         <TextInput name="email" inputType="email" />
         <TextInput name="telefone" inputType="tel" />
     </Card>
+
+    <Card title="Links de Portifólio" icon={LinkDocIcon}>
+    </Card>
+
 </form>
 <Footer />
 
